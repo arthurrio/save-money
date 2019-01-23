74814912f7ecd171172a70782cb16e88d64d9ec8
Getulio Vieira <getuliovdmnaweb@gmail.com>
2019-01-22 23:57:00 -0300

    SM-1 The Road to the Top: Deu Bug em tudo.

---
+++ src/main/java/com/github/savemoney/SavemoneyApplication.java
@@ -0,0 +1,14 @@
+package com.github.savemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavemoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemoneyApplication.class, args);
+	}
+
+}
+

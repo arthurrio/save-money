65bf444ea2f7f22cd2f141cae314009854563bcd
Getulio Vieira <getuliovdmnaweb@gmail.com>
2019-01-23 01:55:53 -0300

    SM-1 Understanding Annotations

---
+++ src/main/java/com/github/savemoney/com/github/savemoney/controller/HelloWorldController.java
@@ -0,0 +1,49 @@
+package com.github.savemoney.com.github.savemoney.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+
+
+public class HelloWorldController {
+
+    @RequestMapping(value = "/helloWorld")
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello World! Road to the top.";
+    }
+
+    @PostMapping("/helloWorldCrazy")
+    @ResponseBody
+    public String helloWorldCrazy(@RequestBody Test name ){
+        return "Hello World! Crazy Road to the top." + name.getName1()+ name.getName();
+    }
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return "Index";
+    }
+}
+class Test{
+
+    private String name;
+    private String name1;
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
